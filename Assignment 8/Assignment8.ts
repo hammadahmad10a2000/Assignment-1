4117aa0b29e90b6f612ef9eb806f4bcf45623549
HAMMAD AHMAD <109570334+hammadahmad10a2000@users.noreply.github.com>
2022-10-10 01:01:55 +0500

    Add files via upload

---
+++ Assignment 8/Assignment8.ts
@@ -0,0 +1,44 @@
+// 1. Write a ts program to read and print elements of array.
+
+// var nums:number[] = [1,2,3,4,5]
+// console.log(nums);
+// var nums:number[] = [1,2,3,4,5]
+// console.log(nums[0])
+
+// 2. Write a ts program to print all negative elements in an array.
+
+
+// 3. Write a ts program to find sum of all array elements.
+
+// var total = [0, 1, 2, 3].reduce(function(a, b){ return a + b; });
+// console.log("total is : " + total );
+
+
+// 4. Write a ts program to find maximum and minimum element in an array.
+
+// var abc:number[] = [100, 0, 50]
+// console.log(Math.min(100,10,0))
+// console.log(Math.max(100,10,0))
+
+
+// 5. Write a ts program to find second largest element in an array.
+
+// function findSecondLargestElement(arr){
+//     let first = -1 , second = -1;
+
+//     for(let i = 0; i <= arr.length-1; i++){
+//         if(arr[i] > first){
+//             second = first;
+//             first = arr[i];
+//         }
+//         else if( arr[i] > second && arr[i] != first){
+//             second = arr[i];
+//         }
+//     }
+//     console.log(second);
+// }
+// let array = [10,20,30,40,50]
+// findSecondLargestElement(array);
+
+// 6. Write a ts program to count total number of even and odd elements in an array.
+
